a1ac2f57b6ad1af5c47fd4225a88b12d5fc55f51
AAKASH HANDA <AAKASH HANDA>
2017-06-06 20:03:16 +0530

    A

---
+++ javascript/third.js
@@ -0,0 +1,10 @@
+var a = document.URL
+undefined
+var b  = a.split("/")[3]
+undefined
+b
+"watch?v=xbAEUGhAubU&t=100s"
+b.split("?")
+(2) ["watch", "v=xbAEUGhAubU&t=100s"]
+b.split("?")[1]
+"v=xbAEUGhAubU&t=100s"
